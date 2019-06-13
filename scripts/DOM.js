19e927e64b7c15c5ffe2fe819f1bd11b8df9d6b9
David Queen <dsqjr@msn.com>
2019-06-13 16:37:10 -0400

    added a modal

--- scripts/DOM.js
+++ scripts/DOM.js
@@ -9,18 +9,38 @@ const DOM =(function(){
     //event handle for bookmark expansion
     $('.js-list').on('click','.js-list-item',(e)=>{
       collection.focused = $(e.currentTarget).data('book-id');
-      console.log(collection.focused);
+      //console.log(collection.focused);
       render();
       $(e.target).find('article').toggleClass('hidden');
     });
   }
-  function hookUpEvents(){
-    handleExpandBookMark();
+  function handleAddBtn(){
+    $('.js-addBtn').click((e)=>{
+      collection.adding = true;
+      // to be removed later
+      $('.modal').toggle();
+      //---->
+      console.log(collection.adding);
+    });
 
   }
+  function handleRemoveBtn(){
+    //event for if the removed button is pressed
+    $('.js-list').on('click','.js-remove',(e)=>{
+      e.stopPropagation();
+      let bookmark = $(e.currentTarget).closest('li').data('book-id');//id of book to remove
+      collection.DeleteBook(bookmark);
+      render();
+    });
+  }
+  function hookUpEvents(){
+    handleRemoveBtn();
+    handleExpandBookMark();
+    handleAddBtn();  
+  }
   function render(){
-    //draw everything in the store
-    console.log('drawing new content');
+    //draw everything in the collection
+    console.log('rendering');
     let html = collection.books.map(book=>createHTML(book));
     $('.js-list').html(html);
   }
@@ -28,15 +48,15 @@ const DOM =(function(){
     if(book.id === collection.focused){
       return ` <li class="js-list-item row" data-book-id="${book.id}">
       <div class="col-12">
-    <span>${book.name}<button class="js-remove">remove</button></span>
+    ${book.name}<button class="js-remove">remove</button>
       <p>rating:${book.rating}</p>
       <article>Description:<br><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac enim facilisis, condimentum augue at, aliquet ex. Etiam at velit eu lectus sagittis ultrices eu sit amet dolor. Integer hendrerit bibendum tortor blandit fringilla.</p></article>
       <a href="#">link to book website</a>
       </div>
     </li>`;
-    }else{return ` <li class="js-list-item row" data-book-id="${book.id}">
-    <div class="col-12">
-  <span>${book.name}<button class="js-remove">remove</button></span>
+    }else{
+      return ` <li class="js-list-item row" data-book-id="${book.id}">
+    <div class="col-12">${book.name}<button class="js-remove">remove</button>
     <p>rating:${book.rating}</p>
     <article class="hidden">Description:<br><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac enim facilisis, condimentum augue at, aliquet ex. Etiam at velit eu lectus sagittis ultrices eu sit amet dolor. Integer hendrerit bibendum tortor blandit fringilla.</p></article>
     <a href="#">link to book website</a>
--- scripts/collection.js
+++ scripts/collection.js
@@ -4,20 +4,30 @@
 'use strict';
 
 const collection = (function(){
-  let books =[new book('Mobey Dick',5),new book('Stuwart little',4)];//todo currently hard coded
+  const books =[new book('Mobey Dick',5),new book('Stuwart little',4)];//todo currently hard coded
   let focused = null;//set to the book that has focus
+  let adding = false;
   /**
    * 
-   * @param {number} ID
+   * @param {string} ID
    * @returns {number} index of book in books 
    * @summary search the books array and gives the index of book with this id
    */
   function findByID(ID){
     return this.books.indexOf(this.books.find((book)=>book.id === ID));
   }
+  /**
+   * @param {string} ID 
+   */
+  function DeleteBook(ID){
+    //given an id, cleanly remove that book from list
+    this.books.splice(this.findByID(ID),1);
+  }
   return{
     books,
     focused,
+    DeleteBook,
     findByID,
+    adding,
   };
 })();
